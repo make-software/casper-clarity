99b2429524be392fd947cbb3be657ede28009f85
Abner Zheng <abnerzheng@gmail.com>
2020-09-28 22:15:43 +0800

    ECO-650: update Block detail page.
    Signed-off-by: Abner Zheng <abnerzheng@gmail.com>

--- packages/ui/src/rpc/BalanceServiceByJsonRPC.ts
+++ packages/ui/src/rpc/BalanceServiceByJsonRPC.ts
@@ -14,22 +14,22 @@ export default class BalanceServiceByJsonRPC {
    * It will cache balance URef values for accounts so that on subsequent queries,
    * it only takes 1 state query not 4 to get the value.
    * @param blockHashBase16
-   * @param accountPublicKeyHash
+   * @param accountPublicKeyHashBase16
    */
   async getAccountBalance(
     blockHashBase16: string,
-    accountPublicKeyHashbase16: string
+    accountPublicKeyHashBase16: string
   ): Promise<number | undefined> {
-    let balanceUref = this.balanceUrefs.get(accountPublicKeyHashbase16);
+    let balanceUref = this.balanceUrefs.get(accountPublicKeyHashBase16);
 
     // Find the balance Uref and cache it if we don't have it.
     if (!balanceUref) {
       balanceUref = await this.casperService.getAccountBalanceUref(
         blockHashBase16,
-        accountPublicKeyHashbase16
+        accountPublicKeyHashBase16
       );
       if (balanceUref) {
-        this.balanceUrefs.set(accountPublicKeyHashbase16, balanceUref);
+        this.balanceUrefs.set(accountPublicKeyHashBase16, balanceUref);
       }
     }
 
