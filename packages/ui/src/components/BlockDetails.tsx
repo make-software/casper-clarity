8539cae50980286932a269e1ffaf084615334565
Abner Zheng <abnerzheng@gmail.com>
2020-09-29 21:35:13 +0800

    ECO-650: update BlockDetails table.
    Signed-off-by: Abner Zheng <abnerzheng@gmail.com>

--- packages/ui/src/components/BlockDetails.tsx
+++ packages/ui/src/components/BlockDetails.tsx
@@ -12,11 +12,10 @@ import {
   FailIcon,
   CSPR
 } from './Utils';
-import { BlockDAG } from './BlockDAG';
 import { Block } from 'casperlabs-grpc/io/casperlabs/casper/consensus/consensus_pb';
 import { shortHash } from './Utils';
 import ObservableValueMap, { ObservableValue } from '../lib/ObservableValueMap';
-import { decodeBase16, encodeBase16 } from 'casperlabs-sdk';
+import { GetDeployResult, JsonBlock } from '../rpc/CasperServiceByJsonRPC';
 
 // https://www.pluralsight.com/guides/react-router-typescript
 
@@ -33,7 +32,7 @@ interface Props extends RouteComponentProps<Params> {
 class _BlockDetails extends RefreshableComponent<Props, {}> {
   constructor(props: Props) {
     super(props);
-    this.props.block.init(decodeBase16(this.blockHashBase16));
+    this.props.block.init(this.blockHashBase16);
   }
 
   get blockHashBase16() {
@@ -46,7 +45,6 @@ class _BlockDetails extends RefreshableComponent<Props, {}> {
 
   async refresh() {
     await this.container.loadBlock();
-    await this.container.loadNeighborhood();
     await this.container.loadDeploys();
     await this.container.loadBalances();
   }
@@ -54,7 +52,7 @@ class _BlockDetails extends RefreshableComponent<Props, {}> {
   componentDidUpdate() {
     // Container and component stays the same during navigation.
     if (this.blockHashBase16 !== this.container.blockHashBase16) {
-      this.container.init(decodeBase16(this.blockHashBase16));
+      this.container.init(this.blockHashBase16);
       this.refresh();
     }
   }
@@ -72,26 +70,6 @@ class _BlockDetails extends RefreshableComponent<Props, {}> {
           deploys={this.container.deploys}
           balances={this.container.balances}
         />
-        <BlockDAG
-          title={`Neighborhood of block ${this.blockHashBase16}`}
-          refresh={() => this.container.loadNeighborhood()}
-          blocks={this.container.neighborhood}
-          hideBallotsToggleStore={this.container.hideBallotsToggleStore}
-          width="100%"
-          height="400px"
-          selected={this.container.block!}
-          depth={this.container.depth}
-          onSelected={block => {
-            let target = encodeBase16(block.getSummary()!.getBlockHash_asU8());
-            if (target !== this.blockHashBase16) {
-              this.props.history.push(Pages.block(target));
-            }
-          }}
-          onDepthChange={depth => {
-            this.container.depth = depth;
-            this.container.loadNeighborhood();
-          }}
-        />
       </div>
     );
   }
@@ -103,7 +81,7 @@ export const BlockDetails = withRouter(_BlockDetails);
 const BlockTable = observer(
   (props: {
     blockHashBase16: string;
-    block: BlockInfo | null;
+    block: JsonBlock | null;
     refresh: () => void;
   }) => {
     const attrs = props.block && blockAttrs(props.block);
@@ -127,7 +105,7 @@ const BlockTable = observer(
 const DeploysTable = observer(
   (props: {
     blockHashBase16: string;
-    deploys: Block.ProcessedDeploy[] | null;
+    deploys: GetDeployResult[] | null;
     balances: ObservableValueMap<string, number>;
   }) => {
     return (
@@ -136,38 +114,38 @@ const DeploysTable = observer(
         headers={[
           'Deploy Hash',
           'Account',
-          'Stage',
+          'Gas Price',
           'Cost',
           'Remaining Balance',
-          'Result',
           'Message'
         ]}
         rows={props.deploys}
         renderRow={(deploy, i) => {
-          const id = encodeBase16(deploy.getDeploy()!.getDeployHash_asU8());
-          const accountId = encodeBase16(
-            deploy
-              .getDeploy()!
-              .getHeader()!
-              .getAccountPublicKeyHash_asU8()
-          );
+          const id = deploy.deploy.hash;
+          const accountId = deploy.deploy.header.account;
           return (
             <tr key={i}>
               <td>
                 <Link to={Pages.deploy(id)}>{shortHash(id)}</Link>
               </td>
               <td>{shortHash(accountId)}</td>
-              <td>{deploy.getStage()}</td>
               <td className="text-right">
-                <CSPR motes={deploy.getCost()} />
+                <CSPR motes={deploy.deploy.header.gas_price} />
+              </td>
+              <td className="text-right">
+                <CSPR motes={deploy.execution_results[0].result.cost} />
               </td>
               <td className="text-right">
                 <Balance balance={props.balances.get(accountId)} />
               </td>
               <td className="text-center">
-                {deploy.getIsError() ? <FailIcon /> : <SuccessIcon />}
+                {deploy.execution_results[0].result.error_message ? (
+                  <FailIcon />
+                ) : (
+                  <SuccessIcon />
+                )}
               </td>
-              <td>{deploy.getErrorMessage()}</td>
+              <td>{deploy.execution_results[0].result.error_message}</td>
             </tr>
           );
         }}
@@ -176,83 +154,27 @@ const DeploysTable = observer(
   }
 );
 
-const blockAttrs: (block: BlockInfo) => Array<[string, any]> = (
-  block: BlockInfo
+const blockAttrs: (block: JsonBlock) => Array<[string, any]> = (
+  block: JsonBlock
 ) => {
-  const id = encodeBase16(block.getSummary()!.getBlockHash_asU8());
-  const header = block.getSummary()!.getHeader()!;
-  const validatorId = encodeBase16(header.getValidatorPublicKeyHash_asU8());
-  const stats = block.getStatus()!.getStats()!;
+  const id = block.hash;
+  const header = block.header;
+  const proposer = header.proposer;
   return [
     ['Block Hash', id],
-    [
-      'Key Block Hash',
-      <Link to={Pages.block(encodeBase16(header.getKeyBlockHash_asU8()))}>
-        {shortHash(header.getKeyBlockHash_asU8())}
-      </Link>
-    ],
-    ['j-Rank', header.getJRank()],
-    ['m-Rank', header.getMainRank()],
-    ['Round ID', header.getRoundId()],
-    ['Timestamp', new Date(header.getTimestamp()).toISOString()],
-    ['Type', <BlockType header={header} />],
-    ['Role', <BlockRole header={header} />],
-    ['Validator', validatorId],
-    ['Validator Block Number', header.getValidatorBlockSeqNum()],
-    [
-      'Validator Stake',
-      (() => {
-        let validatorBond = header
-          .getState()!
-          .getBondsList()
-          .find(
-            x =>
-              encodeBase16(x.getValidatorPublicKeyHash_asU8()) === validatorId
-          );
-        // Genesis doesn't have a validator.
-        return (
-          (validatorBond &&
-            validatorBond.getStake() &&
-            Number(validatorBond.getStake()!.getValue()).toLocaleString()) ||
-          null
-        );
-      })()
-    ],
-    ['Deploy Count', header.getDeployCount()],
-    ['Deploy Error Count', stats.getDeployErrorCount()],
-    ['Deploy Cost Total', <CSPR motes={stats.getDeployCostTotal()} />],
-    ['Deploy Gas Price Average', <CSPR motes={stats.getDeployGasPriceAvg()} />],
-    ['Block Size (bytes)', stats.getBlockSizeBytes().toLocaleString()],
-    ['Finality', <FinalityIcon block={block} />],
-    [
-      'Parents',
-      <ul>
-        {header.getParentHashesList_asU8().map((x, idx) => (
-          <li key={idx}>
-            <BlockLink blockHash={x} />
-          </li>
-        ))}
-      </ul>
-    ],
-    [
-      'Children',
-      <ul>
-        {block
-          .getStatus()!
-          .getChildHashesList_asU8()
-          .map((x, idx) => (
-            <li key={idx}>
-              <BlockLink blockHash={x} />
-            </li>
-          ))}
-      </ul>
-    ]
+    ['Height', header.height],
+    ['Era ID', header.era_id],
+    ['Timestamp', new Date(header.timestamp).toISOString()],
+    ['Proposer', proposer],
+    ['Deploy Count', header.deploy_hashes.length],
+    ['Parent Hash', <BlockLink blockHashBase16={header.parent_hash} />]
   ];
 };
 
-export const BlockLink = (props: { blockHash: ByteArray }) => {
-  let id = encodeBase16(props.blockHash);
-  return <Link to={Pages.block(id)}>{id}</Link>;
+export const BlockLink = (props: { blockHashBase16: string }) => {
+  return (
+    <Link to={Pages.block(props.blockHashBase16)}>{props.blockHashBase16}</Link>
+  );
 };
 
 // Need to observe the balance to react to when it's available.
