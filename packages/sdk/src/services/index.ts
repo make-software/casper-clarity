aa8ecd1fbf6ac7104f33cdf9e8f6bee046ec7cab
Abner Zheng <abnerzheng@gmail.com>
2020-11-18 00:20:59 +0800

    NO_TICKET_REMOVE_GRPC: remove GRPC related code.
    Signed-off-by: Abner Zheng <abnerzheng@gmail.com>

--- packages/sdk/src/services/index.ts
+++ packages/sdk/src/services/index.ts
@@ -1,9 +1,4 @@
-import BalanceService from './BalanceService';
-import CasperService from './CasperService';
-import DiagnosticsService from './DiagnosticsService';
-export { CasperService, BalanceService, DiagnosticsService };
 export { GrpcError } from './Errors';
-export { SubscribeTopics } from './CasperService';
 export * from './CasperServiceByJsonRPC';
 export * from './BalanceServiceByJsonRPC';
 export * from './EventService';
