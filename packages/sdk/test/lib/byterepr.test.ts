019f44eb779e90c6de9f944302c9081e576c2562
Abner Zheng <abnerzheng@gmail.com>
2020-12-24 00:08:23 +0800

    ECO-798: update failed tests

--- packages/sdk/test/lib/byterepr.test.ts
+++ packages/sdk/test/lib/byterepr.test.ts
@@ -1,5 +1,6 @@
 import { expect } from 'chai';
 import {
+  CLTypedAndToBytesHelper,
   decodeBase16,
   I32,
   I64,
@@ -27,17 +28,20 @@ import {
   toBytesU8,
   toBytesVecT
 } from '../../src/lib/byterepr';
-import { BigNumber } from '@ethersproject/bignumber';
 
 describe(`numbers' toBytes`, () => {
   it('should be able to encode u8', () => {
     let bytesU8 = toBytesU8(10);
     expect(bytesU8).to.deep.eq(Uint8Array.from([0x0a]));
-    expect(U8.fromBytes(bytesU8).value).to.eq(10);
+    expect(U8.fromBytes(bytesU8).value.toBytes()).to.deep.eq(
+      CLTypedAndToBytesHelper.u8(10).toBytes()
+    );
 
     bytesU8 = toBytesU8(255);
     expect(bytesU8).to.deep.eq(Uint8Array.from([0xff]));
-    expect(U8.fromBytes(bytesU8).value).to.eq(255);
+    expect(U8.fromBytes(bytesU8).value.toBytes()).to.deep.eq(
+      CLTypedAndToBytesHelper.u8(255).toBytes()
+    );
 
     expect(() => toBytesU8(256)).to.throws('out');
   });
@@ -45,28 +49,42 @@ describe(`numbers' toBytes`, () => {
   it('should be able to encode u32', () => {
     let bytesU32 = toBytesU32(0xf0e0_d0c0);
     expect(bytesU32).to.deep.eq(Uint8Array.from([0xc0, 0xd0, 0xe0, 0xf0]));
-    expect(U32.fromBytes(bytesU32).value).to.eq(0xf0e0_d0c0);
+    expect(U32.fromBytes(bytesU32).value.toBytes()).to.deep.eq(
+      CLTypedAndToBytesHelper.u32(0xf0e0_d0c0).toBytes()
+    );
     bytesU32 = toBytesU32(100000);
     expect(bytesU32).to.deep.eq(Uint8Array.from([160, 134, 1, 0]));
-    expect(U32.fromBytes(bytesU32).value).to.eq(100000);
+    expect(U32.fromBytes(bytesU32).value.toBytes()).to.deep.eq(
+      CLTypedAndToBytesHelper.u32(100000).toBytes()
+    );
     bytesU32 = toBytesU32(0);
     expect(bytesU32).to.deep.eq(Uint8Array.from([0, 0, 0, 0]));
-    expect(U32.fromBytes(bytesU32).value).to.eq(0);
+    expect(U32.fromBytes(bytesU32).value.toBytes()).to.deep.eq(
+      CLTypedAndToBytesHelper.u32(0).toBytes()
+    );
   });
 
   it('should be able to encode i32', () => {
     let bytesI32 = toBytesI32(-100000);
     expect(bytesI32).to.deep.eq(Uint8Array.from([96, 121, 254, 255]));
-    expect(I32.fromBytes(bytesI32).value).to.eq(-100000);
+    expect(I32.fromBytes(bytesI32).value.toBytes()).to.deep.eq(
+      CLTypedAndToBytesHelper.i32(-100000).toBytes()
+    );
     bytesI32 = toBytesI32(100000);
     expect(bytesI32).to.deep.eq(Uint8Array.from([160, 134, 1, 0]));
-    expect(I32.fromBytes(bytesI32).value).to.eq(100000);
+    expect(I32.fromBytes(bytesI32).value.toBytes()).to.deep.eq(
+      CLTypedAndToBytesHelper.i32(100000).toBytes()
+    );
     bytesI32 = toBytesI32(0);
     expect(bytesI32).to.deep.eq(Uint8Array.from([0, 0, 0, 0]));
-    expect(I32.fromBytes(bytesI32).value).to.eq(0);
+    expect(I32.fromBytes(bytesI32).value.toBytes()).to.deep.eq(
+      CLTypedAndToBytesHelper.i32(0).toBytes()
+    );
     bytesI32 = toBytesI32(-1);
     expect(bytesI32).to.deep.eq(Uint8Array.from([255, 255, 255, 255]));
-    expect(I32.fromBytes(bytesI32).value).to.eq(-1);
+    expect(I32.fromBytes(bytesI32).value.toBytes()).to.deep.eq(
+      CLTypedAndToBytesHelper.i32(-1).toBytes()
+    );
   });
 
   it('should be able to encode i64', () => {
@@ -74,15 +92,15 @@ describe(`numbers' toBytes`, () => {
     expect(bytesI64).to.deep.eq(
       Uint8Array.from([57, 20, 94, 139, 1, 121, 193, 2])
     );
-    expect(I64.fromBytes(bytesI64).value).to.deep.equal(
-      BigNumber.from('198572906121139257')
+    expect(I64.fromBytes(bytesI64).value.toBytes()).to.deep.eq(
+      CLTypedAndToBytesHelper.i64('198572906121139257').toBytes()
     );
     bytesI64 = toBytesI64('-4009477689550808');
     expect(bytesI64).to.deep.eq(
       Uint8Array.from([40, 88, 148, 186, 102, 193, 241, 255])
     );
-    expect(I64.fromBytes(bytesI64).value).to.deep.equal(
-      BigNumber.from('-4009477689550808')
+    expect(I64.fromBytes(bytesI64).value.toBytes()).to.deep.equal(
+      CLTypedAndToBytesHelper.i64('-4009477689550808').toBytes()
     );
   });
 
@@ -91,37 +109,37 @@ describe(`numbers' toBytes`, () => {
     expect(bytesU64).to.deep.eq(
       Uint8Array.from([57, 20, 214, 178, 212, 118, 11, 197])
     );
-    expect(U64.fromBytes(bytesU64).value).to.deep.equal(
-      BigNumber.from('14198572906121139257')
+    expect(U64.fromBytes(bytesU64).value.toBytes()).to.deep.equal(
+      CLTypedAndToBytesHelper.u64('14198572906121139257').toBytes()
     );
     bytesU64 = toBytesU64('9834009477689550808');
     expect(bytesU64).to.deep.eq(
       Uint8Array.from([216, 167, 130, 99, 132, 107, 121, 136])
     );
-    expect(U64.fromBytes(bytesU64).value).to.deep.equal(
-      BigNumber.from('9834009477689550808')
+    expect(U64.fromBytes(bytesU64).value.toBytes()).to.deep.equal(
+      CLTypedAndToBytesHelper.u64('9834009477689550808').toBytes()
     );
   });
 
   it('should be able to encode u128', () => {
     let bytesU128 = toBytesU128(100000);
     expect(bytesU128).to.deep.eq(Uint8Array.from([3, 160, 134, 1]));
-    expect(U128.fromBytes(bytesU128).value).to.deep.equal(
-      BigNumber.from(100000)
+    expect(U128.fromBytes(bytesU128).value.toBytes()).to.deep.equal(
+      CLTypedAndToBytesHelper.u128(100000).toBytes()
     );
     bytesU128 = toBytesU128(0xf0e0_d0c0_0000);
     expect(bytesU128).to.deep.eq(
       Uint8Array.from([6, 0, 0, 0xc0, 0xd0, 0xe0, 0xf0])
     );
-    expect(U128.fromBytes(bytesU128).value).to.deep.equal(
-      BigNumber.from(0xf0e0_d0c0_0000)
+    expect(U128.fromBytes(bytesU128).value.toBytes()).to.deep.equal(
+      CLTypedAndToBytesHelper.u128(0xf0e0_d0c0_0000).toBytes()
     );
     bytesU128 = toBytesU128(0x0000_f0e0_d0c0_0000);
     expect(bytesU128).to.deep.eq(
       Uint8Array.from([6, 0, 0, 0xc0, 0xd0, 0xe0, 0xf0])
     );
-    expect(U128.fromBytes(bytesU128).value).to.deep.equal(
-      BigNumber.from(0x0000_f0e0_d0c0_0000)
+    expect(U128.fromBytes(bytesU128).value.toBytes()).to.deep.equal(
+      CLTypedAndToBytesHelper.u128(0x0000_f0e0_d0c0_0000).toBytes()
     );
   });
 
