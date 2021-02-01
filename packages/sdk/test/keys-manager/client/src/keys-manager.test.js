281722229b9178eec71fbb91f1f665d831a707c5
George <george@casperlabs.io>
2021-02-01 22:07:45 +0000

    Continued WIP

--- packages/sdk/test/keys-manager/client/src/keys-manager.test.js
+++ packages/sdk/test/keys-manager/client/src/keys-manager.test.js
@@ -9,33 +9,40 @@ describe('Testing SDK', () => {
         this.firstAccount = masterKey.deriveIndex(2);
         this.secondAccount = masterKey.deriveIndex(3);
 
-
     });
 
     it('Should fund account', async () => {
+        // Account needs to be funded before getAccount can be called
+        await utils.fund(this.mainAccount, 1000);
         let account = await utils.getAccount(this.mainAccount.publicKey);
-        let initialBalance = account.mainPurse;
-        await utils.fund(this.mainAccount);
-        let finalBalance = account.mainPurse;
 
+        let initialBalance = await utils.getBalanceOfByAccountHash(utils.toAccountHashString(this.mainAccount.publicKey));
+        console.log(`Initial Balance: ${initialBalance}`);
+
+        await utils.fund(this.mainAccount, 100);
+
+        let finalBalance =  await utils.getBalanceOfByAccountHash(utils.toAccountHashString(this.mainAccount.publicKey));
+        console.log(`Final Balance: ${finalBalance}`);
+
+        // Balance should be 1100
         chai.assert(finalBalance > initialBalance, "Balance did not increase");
     });
     
-    // it('Should make a transfer...', async () => {
+    it('Should make a transfer...', async () => {
 
-    //     let deployThreshold = 2;
-    //     let keyManagementThreshold = 2;
-    //     let accounts = [
-    //         { publicKey: this.mainAccount.publicKey, weight: 1 },
-    //         { publicKey: this.firstAccount.publicKey, weight: 1 }, 
-    //     ];
+        let deployThreshold = 2;
+        let keyManagementThreshold = 2;
+        let accounts = [
+            { publicKey: this.mainAccount.publicKey, weight: 1 },
+            { publicKey: this.firstAccount.publicKey, weight: 1 }, 
+        ];
 
-    //     let deploy = utils.keys.setAll(this.mainAccount, deployThreshold, keyManagementThreshold, accounts);
-    //     await utils.sendDeploy(deploy, [this.mainAccount]);
+        let deploy = utils.keys.setAll(this.mainAccount, deployThreshold, keyManagementThreshold, accounts);
+        await utils.sendDeploy(deploy, [this.mainAccount]);
 
-    //     deploy = utils.transferDeploy(this.mainAccount, this.secondAccount, 1000);
-    //     await utils.sendDeploy(deploy, [this.mainAccount, this.firstAccount]);
+        deploy = utils.transferDeploy(this.mainAccount, this.secondAccount, 1000);
+        await utils.sendDeploy(deploy, [this.mainAccount, this.firstAccount]);
 
-    //     chai.assert();
-    // });
+        chai.assert();
+    });
 });
