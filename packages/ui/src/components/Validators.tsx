bdf51a3b50fc33ab01378dfd1a3dde9114bb7765
Michael Steuer <michael@make.services>
2021-02-10 16:01:17 -0800

    Adding stake percentage to Validator Bids tab, and Validator Era tabs

--- packages/ui/src/components/Validators.tsx
+++ packages/ui/src/components/Validators.tsx
@@ -48,138 +48,152 @@ const navContent = (isActive: boolean, tabName: string, dataTable: any) => {
 
 @observer
 export default class Validators extends RefreshableComponent<Props, {}> {
-  async refresh() {
-    this.props.validatorsContainer.refresh();
-  }
-
-  render() {
-    // If data is not there, display loader.
-    let data = this.props.validatorsContainer.validatorsInfo;
-    if (!data) {
-      return <Loading />;
+    async refresh() {
+        this.props.validatorsContainer.refresh();
     }
 
-    // Contaners for tabs.
-    let navs = [];
-    let tables = [];
+    render() {
+        // If data is not there, display loader.
+        let data = this.props.validatorsContainer.validatorsInfo;
 
-    // Prepare bids navigation element.
-    navs.push(
-      navElement(true, 'validators-bids-nav', 'validators-bids-tab', 'Bids')
-    );
+        if (!data) {
+            return <Loading/>;
+        }
 
-    // Prepare bids rows.
-    let rows = data.bids
-      .map(bid => {
-        let stake = bid.bid.staked_amount;
-        return {
-          validatorId: bid.public_key,
-          delegation_rate: bid.bid.delegation_rate,
-          stakeStr: stake,
-          stakeNum: BigNumber.from(stake),
-          reward: bid.bid.reward
-        };
-      })
-      .sort((a, b) => compareBigNumbers(a.stakeNum, b.stakeNum));
+        let sum = data.bids.reduce((acc, val) => acc + BigNumber.from(val.bid.staked_amount).div(BigNumber.from(1000_000_000)).toNumber(), 0);
 
-    // Build bids data table.
-    tables.push(
-      navContent(
-        true,
-        'validators-bids-tab',
-        <DataTable
-          title="Bids"
-          headers={['Validator ID', 'Slot', 'Delegation Rate', 'Stake']}
-          rows={rows}
-          renderRow={(bidInfo, index) => {
-            let key = `bids-${bidInfo.validatorId}`;
-            return (
-              <tr key={key}>
-                <td>{bidInfo.validatorId}</td>
-                <td>{index! + 1}</td>
-                <td>{bidInfo.delegation_rate}</td>
-                <td>
-                  <CSPR motes={bidInfo.stakeNum} />
-                </td>
-              </tr>
+        // Contaners for tabs.
+        let navs = [];
+        let tables = [];
+
+        // Prepare bids navigation element.
+        navs.push(
+            navElement(true, 'validators-bids-nav', 'validators-bids-tab', 'Bids')
+        );
+
+
+        // Prepare bids rows.
+        let rows = data.bids
+            .map(bid => {
+                let stake = bid.bid.staked_amount;
+                return {
+                    validatorId: bid.public_key,
+                    delegation_rate: bid.bid.delegation_rate,
+                    stakeStr: stake,
+                    stakeNum: BigNumber.from(stake),
+                    reward: bid.bid.reward,
+                    stakePerc: ((BigNumber.from(stake).div(BigNumber.from(1000_000_000)).toNumber() / sum) * 100).toFixed(2)
+                };
+            })
+            .sort((a, b) => compareBigNumbers(a.stakeNum, b.stakeNum));
+
+        // Build bids data table.
+        tables.push(
+            navContent(
+                true,
+                'validators-bids-tab',
+                <DataTable
+                    title="Bids"
+                    headers={['Validator ID', 'Slot', 'Delegation Rate', 'Stake']}
+                    rows={rows}
+                    renderRow={(bidInfo, index) => {
+                        let key = `bids-${bidInfo.validatorId}`;
+                        return (
+                            <tr key={key}>
+                                <td>{bidInfo.validatorId}</td>
+                                <td>{index! + 1}</td>
+                                <td>{bidInfo.delegation_rate}</td>
+                                <td>
+                                    <CSPR motes={bidInfo.stakeNum}/>
+                                    <span className="stakePerc">
+                                        ({bidInfo.stakePerc}%)
+                                    </span>
+                                </td>
+                            </tr>
+                        );
+                    }}
+                    noHeader={true}
+                />
+            )
+        );
+
+        // For each era build navigation element and data table.
+        // for (const [index, eraId] of Object.keys(data.era_validators).entries()) {
+        for (const [index, era] of data.era_validators.entries()) {
+            let eraName = `Era ${era.era_id} ${index == 0 ? '(current)' : ''}`;
+            let tabName = `validator-tab-${era.era_id}`;
+            let navName = `${tabName}-nav`;
+
+            // Build era navigation element.
+            navs.push(navElement(false, navName, tabName, eraName));
+
+            let sum = era.validator_weights.reduce((acc, val) => acc + BigNumber.from(val.weight).div(BigNumber.from(1000_000_000)).toNumber(), 0);
+
+            // Prepare era rows.
+            let rows = era.validator_weights
+                .map(validator_weight => {
+                    let stake = validator_weight.weight;
+                    return {
+                        validatorId: validator_weight.public_key,
+                        stakeStr: stake,
+                        stakeNum: BigNumber.from(stake),
+                        stakePerc: ((BigNumber.from(stake).div(BigNumber.from(1000_000_000)).toNumber() / sum) * 100).toFixed(2)
+                    };
+                })
+                .sort((a, b) => compareBigNumbers(a.stakeNum, b.stakeNum));
+
+            // Build era data table.
+            tables.push(
+                navContent(
+                    false,
+                    tabName,
+                    <DataTable
+                        title={eraName}
+                        headers={['Validator ID', 'Slot', 'Stake']}
+                        rows={rows}
+                        renderRow={(validatorInfo, index) => {
+                            let key = `${era.era_id}-${validatorInfo.validatorId}`;
+                            return (
+                                <tr key={key}>
+                                    <td>{validatorInfo.validatorId}</td>
+                                    <td>{index! + 1}</td>
+                                    <td>
+                                        <CSPR motes={validatorInfo.stakeNum}/>
+                                        <span className="stakePerc">
+                                            ({validatorInfo.stakePerc}%)
+                                        </span>
+                                    </td>
+                                </tr>
+                            );
+                        }}
+                        noHeader={true}
+                    />
+                )
             );
-          }}
-          noHeader={true}
-        />
-      )
-    );
+        }
 
-    // For each era build navigation element and data table.
-    // for (const [index, eraId] of Object.keys(data.era_validators).entries()) {
-    for (const [index, era] of data.era_validators.entries()) {
-      let eraName = `Era ${era.era_id} ${index == 0 ? '(current)' : ''}`;
-      let tabName = `validator-tab-${era.era_id}`;
-      let navName = `${tabName}-nav`;
-
-      // Build era navigation element.
-      navs.push(navElement(false, navName, tabName, eraName));
-
-      // Prepare era rows.
-      let rows = era.validator_weights
-        .map(validator_weight => {
-          let stake = validator_weight.weight;
-          return {
-            validatorId: validator_weight.public_key,
-            stakeStr: stake,
-            stakeNum: BigNumber.from(stake)
-          };
-        })
-        .sort((a, b) => compareBigNumbers(a.stakeNum, b.stakeNum));
-
-      // Build era data table.
-      tables.push(
-        navContent(
-          false,
-          tabName,
-          <DataTable
-            title={eraName}
-            headers={['Validator ID', 'Slot', 'Stake']}
-            rows={rows}
-            renderRow={(validatorInfo, index) => {
-              let key = `${era.era_id}-${validatorInfo.validatorId}`;
-              return (
-                <tr key={key}>
-                  <td>{validatorInfo.validatorId}</td>
-                  <td>{index! + 1}</td>
-                  <td>
-                    <CSPR motes={validatorInfo.stakeNum} />
-                  </td>
-                </tr>
-              );
-            }}
-            noHeader={true}
-          />
-        )
-      );
+        return (
+            <div id="validators-tab">
+                <div className="container-fluid p-3 mt-3">
+                    <h4>Validators</h4>
+                </div>
+                <div className="container-fluid">
+                    <ul
+                        className="nav nav-pills mb-3 validator-tab-nav-list"
+                        id="validator-tabs"
+                        role="tablist"
+                    >
+                        {navs}
+                    </ul>
+                </div>
+                <div className="container-fluid">
+                    <div className="tab-content" id="validator-tabsContent">
+                        {tables}
+                    </div>
+                </div>
+            </div>
+        );
     }
-
-    return (
-      <div id="validators-tab">
-        <div className="container-fluid p-3 mt-3">
-          <h4>Validators</h4>
-        </div>
-        <div className="container-fluid">
-          <ul
-            className="nav nav-pills mb-3 validator-tab-nav-list"
-            id="validator-tabs"
-            role="tablist"
-          >
-            {navs}
-          </ul>
-        </div>
-        <div className="container-fluid">
-          <div className="tab-content" id="validator-tabsContent">
-            {tables}
-          </div>
-        </div>
-      </div>
-    );
-  }
 }
 
 function compareBigNumbers(a: BigNumber, b: BigNumber): number {
